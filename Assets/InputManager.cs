454960daf56bd07c0d20f36f86fc22696992704f
Johan Horvius <drhorv@gmail.com>
2020-04-23 18:49:30 +0200

    Rotation locked, and ready

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class InputManager : MonoBehaviour
 {
-    public float panSpeed;
-    public float panDetect = 15f;
+    public float horizontalPanSpeed = 100f;
+    public float verticalPanSpeed = 100f;
     public float rotateSpeed;
     public float rotateFactor;
     private Quaternion _rotation;
     private float _minHeight = 10f;
     private float _maxHeight = 100f;
-    
+    private bool _cursorLocked = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +23,43 @@ public class InputManager : MonoBehaviour
     {
         MoveCamera();
         RotateCamera();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Camera.main.transform.rotation = _rotation;
+        }
     }
 
     private void RotateCamera()
     {
         Vector3 origin = Camera.main.transform.eulerAngles;
         Vector3 destination = origin;
-        if (Input.GetMouseButtonDown(2))
+        if (Input.GetMouseButton(2))
         {
-            destination.x -= Input.GetAxis("Mouse Y") * rotateFactor;
-            destination.y += Input.GetAxis("Mouse X") * rotateFactor;
+            if (!_cursorLocked) {
+                Cursor.lockState = CursorLockMode.Locked;
+                _cursorLocked = true;
+            }
+            var x = destination.x - Input.GetAxis("Mouse Y") * rotateFactor;
+            destination.x = Mathf.Clamp(x, 0, 80);
+            
+            var yOrig = destination.y + Input.GetAxis("Mouse X") * rotateFactor; 
+            if (yOrig < 270 && yOrig > 260)
+            {
+                destination.y = 270;
+            } else if (yOrig > 90 && yOrig < 100)
+            {
+                destination.y = 90;
+            }
+            else
+            {
+                destination.y = yOrig;
+            }
+        } else
+        {
+            if (_cursorLocked) {
+                Cursor.lockState = CursorLockMode.None;
+                _cursorLocked = false;
+            }
         }
 
         if (destination != origin)
@@ -45,7 +71,7 @@ public class InputManager : MonoBehaviour
     private void MoveCamera()
     {
         var delta = Time.deltaTime;
-        var currentPanSpeed = delta * panSpeed;
+        var currentPanSpeed = delta * horizontalPanSpeed;
         Camera camera = Camera.main;
         float moveX = camera.transform.position.x;
         float moveZ = camera.transform.position.z;
@@ -53,24 +79,27 @@ public class InputManager : MonoBehaviour
 
         float mouseX = Input.mousePosition.x;
         float mouseY = Input.mousePosition.y;
-        
-        if (Input.GetKey(KeyCode.A)  )
+
+        if (Input.GetKey(KeyCode.A))
         {
             moveX -= currentPanSpeed;
-        } else if (Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             moveX += currentPanSpeed;
         }
-        
+
         if (Input.GetKey(KeyCode.W))
         {
             moveZ += currentPanSpeed;
-        } else if (Input.GetKey(KeyCode.S))
+        }
+        else if (Input.GetKey(KeyCode.S))
         {
             moveZ -= currentPanSpeed;
         }
 
-        
+        moveY -= Input.GetAxis("Mouse ScrollWheel") * currentPanSpeed * verticalPanSpeed;
+        moveY = Mathf.Clamp(moveY, _minHeight, _maxHeight);
         Vector3 newPos = new Vector3(moveX, moveY, moveZ);
         camera.transform.position = newPos;
     }
